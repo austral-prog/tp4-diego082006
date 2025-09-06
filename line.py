2f3488f9480a68c3bbdb6d1137323a611332b313
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-09-06 15:18:21 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,6 +1,7 @@
 def line():
     print("TO DO")
-    
+
+    # Coeficientes
     A = float(input("Ingrese el coeficiente A: "))
     B = float(input("Ingrese el coeficiente B: "))
     X1 = float(input("Ingrese el coeficiente X1: "))
