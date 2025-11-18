bfe1be9073bde5baec683e25032116248397cbd6
diego082006 <ddelgado1@mail.austral.edu.ar>
2025-11-18 18:07:23 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,12 +1,11 @@
 def leap_year():
     year = int(input("Ingrese un año: "))
 
-    # Un año es bisiesto si es divisible por 4, excepto si es divisible por 100
-    # A menos que también sea divisible por 400.
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print(f"El año {year} es bisiesto")
     else:
         print(f"El año {year} no es bisiesto")
 
 
-leap_year()
+if __name__ == "__main__":
+    leap_year()
